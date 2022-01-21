d78b4333101c2ec7003cb8171394525d37dea8ac
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-01-21 14:55:47 +0100

    Basic express srver with typescript + dotev config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Demo GraphQL CRUD');
+import app from "./app";
+import {PORT} from './config'
+
+const main = () => {
+  app.listen(PORT);
+  console.log("Server on port", PORT);
+};
+
+main();
