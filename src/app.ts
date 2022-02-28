00bede231c91cad2363b392545034595426cd226
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-02-28 12:17:39 +0100

    Update app

--- src/app.ts
+++ src/app.ts
@@ -6,4 +6,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", async (_req, res) => {
+  res.send('<h1>Demo Express API with Typescript</h1>');
+});
+
+
+app.get("/ping", async (_req, res) => {
+  res.send({
+    message: "pong",
+  });
+});
+
+
 export default app;
